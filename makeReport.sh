bc2bab496ebf9cc9822813d77ef0c57632f4f6c1
faraz <fahmed02@icloud.com>
2024-05-13 11:40:21 -0400

    init- RNA

---
+++ makeReport.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+display_usage(){
+  echo "------------------------------------------------------------------------------------------------------------------"
+  echo "run the script using the following syntax:"
+  echo "    bash" $0 "<-k3> <Report_Title> <Genome> <Annot>"
+  echo ""
+  echo " -k1  = knit with all headers (including MA-plot)"
+  echo " -k2  = knit with all headers Filtered (2 projects e.g. PCA-flt.png)"
+  echo " -k3  = knit w/o GeneBodyCov"
+  echo " -k4  = knit w/o GeneBodyCov Filtered (2 projects e.g. PCA-flt.png)"
+  echo "------------------------------------------------------------------------------------------------------------------"
+}
+
+T=$2
+G=$3
+A=$4
+
+
+knit_full_report(){
+
+  scp /Users/fa286/Documents/GitHub/quarto-markdowns/qmds/full-report-nf.qmd .
+  
+  quarto render full-report-nf.qmd -P title:$T -P genome:$G -P annot:$A -o ${T}-Report.html
+
+  rm *.qmd
+
+}
+
+knit_full_filtered(){
+
+  scp /Users/fa286/Documents/GitHub/quarto-markdowns/qmds/full-report-nf-filtered.qmd .
+  
+  quarto render full-report-nf-filtered.qmd -P title:$T -P genome:$G -P annot:$A -o ${T}-Report.html
+
+  rm *.qmd
+
+}
+
+
+knit_nogbcov(){
+
+  scp /Users/fa286/Documents/GitHub/quarto-markdowns/qmds/nogbcov-report-nf.qmd .
+  
+  quarto render nogbcov-report-nf.qmd -P title:$T -P genome:$G -P annot:$A -o ${T}-Report.html
+
+  rm *.qmd
+
+}
+
+
+knit_nogbcov_filtered(){
+
+  scp /Users/fa286/Documents/GitHub/quarto-markdowns/qmds/nogbcov-filtered-nf.qmd .
+  
+  quarto render nogbcov-filtered-nf.qmd -P title:$T -P genome:$G -P annot:$A -o ${T}-Report.html
+
+  rm *.qmd
+
+}
+
+
+raise_error() {
+  echo "-------------------------------------------------------------------"
+  local error_message="$@"
+  echo "${error_message}" 1>&2;
+  echo "-------------------------------------------------------------------"
+}
+
+
+
+case $1 in
+    -h|--help)
+      display_usage
+      ;;
+    -k1|--knit1)
+      knit_full_report
+      ;;
+    -k2|--knit2)
+      knit_full_filtered
+      ;;
+    -k3|--knit3)
+      knit_nogbcov
+      ;;
+    -k4|--knit4)
+      knit_nogbcov_filtered
+      ;;
+
+     *)
+      raise_error "Unknown argument(s): ${1}"
+      display_usage
+      ;;
+esac
